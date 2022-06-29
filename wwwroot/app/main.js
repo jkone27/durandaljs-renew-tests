bf5501a3f4f15d18aa25d40c15c4952bc35248db
gparmigiani <gparmigiani@travix.com>
2022-06-29 17:52:14 +0200

    first commit

---
+++ wwwroot/app/main.js
@@ -0,0 +1,27 @@
+requirejs.config({
+    paths: {
+        'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
+        'durandal':'https://cdn.jsdelivr.net/npm/durandal@2.2.0/js',
+        'plugins' : 'https://cdn.jsdelivr.net/npm/durandal@2.2.0/js/plugins',
+        'transitions' : 'https://cdn.jsdelivr.net/npm/durandal@2.2.0/js/transitions',
+        'knockout': 'http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.5.0',
+        'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.1/jquery.min',
+        } 
+    });
+    define(function (require) {
+        var system = require('durandal/system');
+        var app = require('durandal/app');
+
+        system.debug(true);
+
+        app.title = 'Durandal Starter Kit';
+
+        app.configurePlugins({
+            router:true,
+            dialog: true
+        });
+
+        app.start().then(function() {
+            app.setRoot('shell');
+        });
+    });
